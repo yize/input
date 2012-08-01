cee9ef6b4c3f24b5447f3dd5faf615504d0e9744
yize <yize.shc@gmail.com>
2012-08-01 19:00:11 +0800

    自动初始化

--- js/input.js
+++ js/input.js
@@ -1,31 +1,37 @@
 /*
-*  fileOverView:¼æÈÝÖ÷Á÷ä¯ÀÀÆ÷µÄinput:file
+ * fileOverView:¼æÈÝÖ÷Á÷ä¯ÀÀÆ÷µÄinput:file
  * author:yize.shc@gmail.com
  * date:2012/8/1
  * */
 KISSY.ready(function (S) {
-    var D = S.DOM, E = S.Event;
-    var inputClass = '.input-test',
-        inputWrapClass = '.input-wrap',
-        inputWrap = D.query(inputWrapClass);
-    var input = D.query('.input-test', inputWrap);
-    D.css(inputWrap, {'position':'relative', 'cursor':'pointer', 'overflow':'hidden'});
+    S.use('sizzle', function () {
+        var D = S.DOM, E = S.Event;
+        var inputWrap = D.query('[node-type="inputWrap"]');
+        var input = D.query('input:file', inputWrap);
+        S.each(inputWrap, function (e) {
+            if (D.css(e, 'position') !== 'absolute') {
+                console.log(e);
+                D.css(e, 'position', 'relative');
+            }
+        });
+        D.css(inputWrap, {'cursor':'pointer', 'overflow':'hidden'});
+        D.css(input, {'cursor':'pointer', 'position':'absolute', 'width':'100', 'margin':'0', 'filter':'progid:DXImageTransform.Microsoft.Alpha(Opacity=0)', 'opacity':'0', 'clip':'rect(9px,86px,15px,80px)', 'display':'block'});
 
-    D.css(input, {'cursor':'pointer', 'position':'absolute', 'width':'100', 'margin':'0', 'filter':'progid:DXImageTransform.Microsoft.Alpha(Opacity=0)', 'opacity':'0', 'clip':'rect(9px,86px,15px,80px)'});
+        D.attr('hidefocus', 'true');
 
-    D.attr('hidefocus', 'true');
+        E.on(inputWrap, 'mousemove', function (ev) {
+            var currentTarget = ev.currentTarget;
+            var offset = D.offset(currentTarget);
 
-    E.on(inputWrap, 'mousemove', function (ev) {
-        var currentTarget = ev.currentTarget;
-        var offset = D.offset(currentTarget);
-        var input = D.get(inputClass, currentTarget);
-        var width = D.width(input);
-        var left = ev.clientX - D.scrollLeft(window) - offset.left;
-        var top = ev.clientY + D.scrollTop(window) - offset.top;
-        if (S.UA.ie) {
-            D.css(input, {left:left - width + 12, top:top - 12});
-        } else {
-            D.css(input, {left:left - width + 17, top:top - 12});
-        }
-    })
+            var input = D.get('input:file', currentTarget);
+            var width = D.width(input);
+            var left = ev.clientX - D.scrollLeft(window) - offset.left;
+            var top = ev.clientY + D.scrollTop(window) - offset.top;
+            if (S.UA.ie) {
+                D.css(input, {left:left - width + 12, top:top - 12});
+            } else {
+                D.css(input, {left:left - width + 17, top:top - 12});
+            }
+        })
+    });
 });
