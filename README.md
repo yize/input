b3c149d9b524a2fa11576d8e627e6cf9be941b3b
yize <yize.shc@gmail.com>
2012-08-02 09:54:04 +0800

    add README

---
+++ README.md
@@ -0,0 +1,47 @@
+对input的兼容（基于KISSY 1.1.6+）
+=========
+
+原理
+----
+
+将`input:file`控件设置为不可见，并且将其进行切分，当触发鼠标在按钮上的`mousemove`事件时，同时改变input:file空间的定位，使空间一直跟随鼠标移动，这样点击的时候就可以点击到原生的input:file，针对不同浏览器做了兼容
+
+测试过的浏览器：
+
+- IE 5.5+ 
+- Firefox
+- Chrome
+- Safari
+- Opera
+
+用法
+-----
+
+###HTML：
+```html
+<div class="" node-type="inputWrap">
+    <input type="file" size="1" hidefocus="true" style="display: none;">
+</div>
+```
+
+`input`外的`div`节点为自定义样式的按钮,只需要给它设定一个自定义属性`node-type="inputWrap"`
+
+`input:file`的`size="1" hidefocus="true" style="display: none;"`属性为必须
+
+
+###JavaScript：
+
+```js
+<script type="text/javascript" src="http://a.tbcdn.cn/s/kissy/1.2.0/kissy-min.js"></script>
+```
+
+KISSY的版本为1.1.6+
+
+js也可以异步引入
+
+```js
+<script type="text/javascript">
+    KISSY.getScript('http://a.tbcdn.cn/apps/matrix-mission/js/input.js');
+</script>
+```
+
--- js/input.js
+++ js/input.js
@@ -3,20 +3,22 @@
  * author:yize.shc@gmail.com
  * date:2012/8/1
  * */
-KISSY.ready(function (S) {
+
+ KISSY.ready(function (S) {
     S.use('sizzle', function () {
+
         var D = S.DOM, E = S.Event;
         var inputWrap = D.query('[node-type="inputWrap"]');
         var input = D.query('input:file', inputWrap);
+
         S.each(inputWrap, function (e) {
             if (D.css(e, 'position') !== 'absolute') {
-                console.log(e);
                 D.css(e, 'position', 'relative');
             }
         });
-        D.css(inputWrap, {'cursor':'pointer', 'overflow':'hidden'});
-        D.css(input, {'cursor':'pointer', 'position':'absolute', 'width':'100', 'margin':'0', 'filter':'progid:DXImageTransform.Microsoft.Alpha(Opacity=0)', 'opacity':'0', 'clip':'rect(9px,86px,15px,80px)', 'display':'block'});
 
+        D.css(inputWrap, {'cursor':'pointer', 'overflow':'hidden'});
+        D.css(input, {'cursor':'pointer', 'position':'absolute', 'width':'100', 'margin':'0', 'filter':'progid:DXImageTransform.Microsoft.Alpha(Opacity=0)', 'opacity':'0', 'clip':'rect(9px,86px,15px,80px)','display':'block'});
         D.attr('hidefocus', 'true');
 
         E.on(inputWrap, 'mousemove', function (ev) {
