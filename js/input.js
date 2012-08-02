07d710077cabcc4f19be0622280fa4f38db91141
yize <yize.shc@gmail.com>
2012-08-02 11:11:47 +0800

    update input.js

--- js/input.js
+++ js/input.js
@@ -3,8 +3,7 @@
  * author:yize.shc@gmail.com
  * date:2012/8/1
  * */
-
- KISSY.ready(function (S) {
+KISSY.ready(function (S) {
     S.use('sizzle', function () {
 
         var D = S.DOM, E = S.Event;
@@ -18,24 +17,24 @@
         });
 
         D.css(inputWrap, {'cursor':'pointer', 'overflow':'hidden'});
-        D.css(input, {'cursor':'pointer', 'position':'absolute', 'width':'100', 'margin':'0', /*'filter':'progid:DXImageTransform.Microsoft.Alpha(Opacity=0)', 'opacity':'0',*/'clip':'rect(9px,86px,15px,80px)','display':'block'});
-        D.attr('hidefocus', 'true');
+        D.css(input, {'cursor':'pointer', 'position':'absolute', 'width':'100', 'margin':'0', 'filter':'progid:DXImageTransform.Microsoft.Alpha(Opacity=0)', 'opacity':'0', 'clip':'rect(9px,86px,15px,80px)', 'display':'block'});
 
         E.on(inputWrap, 'mousemove', function (ev) {
             var currentTarget = ev.currentTarget;
             var offset = D.offset(currentTarget);
-
             var input = D.get('input:file', currentTarget);
             var width = D.width(input);
             var left = ev.clientX - D.scrollLeft(window) - offset.left;
             var top = ev.clientY + D.scrollTop(window) - offset.top;
-            if (S.UA.ie) {
+
+            if (S.UA.ie < 9) {
                 D.css(input, {left:left - width + 12, top:top - 12});
-            }else if(S.UA.firefox){
-                D.css(input,{left:left - width + 20, top:top - 12})
+            } else if (S.UA.firefox) {
+                D.css(input, {left:left - width + 20, top:top - 12})
             } else {
                 D.css(input, {left:left - width + 17, top:top - 12});
             }
+
         })
     });
 });
